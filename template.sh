a02de836bb8719de3fadd119ad7259edeeb95159
Rion <htmldigger@gmail.com>
2021-03-05 11:06:48 +0530

    Main File v1.2
    Updated Main File to make more efficient

--- template.sh
+++ template.sh
@@ -4,32 +4,59 @@
 # Author : HTMLDigger
 # Date : Saturday 13 February 2021 08:35:55 PM IST
 # Script Name : template
-# Version :
+# Version : v1.2
 # *************************************************************
 # *************************************************************
 
 clear
-
 echo -e "\e[92m"
 DATE=$(date)
+banner() {
+    clear
+    echo -e "╦ ╦╔╦╗╔╦╗╦  ╔╦╗┬┌─┐┌─┐┌─┐┬─┐"
+    echo -e "╠═╣ ║ ║║║║   ║║││ ┬│ ┬├┤ ├┬┘"
+    echo -e "╩ ╩ ╩ ╩ ╩╩═╝═╩╝┴└─┘└─┘└─┘┴└─.in"
+    echo ""
+}
 
-read -p "Please input your script Name : " ansName
-read -p "Please mention Version : " ansVER
-mkdir -p /HTMLDigger/bash
-touch /HTMLDigger/bash/$ansName
+banner
+echo "$DATE"
+echo "****************************************************************************"
+yearp=$(date +"%Y")
+read -p "Script Name : " ansName
+read -p "Script Version : " ansVER
+read -p "Description : " des
+mkdir -p /$HOME/bash/
+touch /$HOME/bash/$ansName
+echo "#! /bin/bash
+# :author                           : $USER
+# :support                          : $USER
+# :type                             : bash
+# :title                            : $ansName
+# :description                      : $des
 
-echo "#! /bin/bash" >> /HTMLDigger/bash/$ansName
-echo "# *************************************************************" >> /HTMLDigger/bash/$ansName
-echo "# *************************************************************" >> /HTMLDigger/bash/$ansName
-echo "# Author : HTMLDigger" >> /HTMLDigger/bash/$ansName
-echo "# Date : $DATE"  >> /HTMLDigger/bash/$ansName
-echo "# Script Name : $ans"  >> /HTMLDigger/bash/$ansName
-echo "# Version : v$ansVER"  >> /HTMLDigger/bash/$ansName
-echo "# *************************************************************" >> /HTMLDigger/bash/$ansName
-echo "# *************************************************************" >> /HTMLDigger/bash/$ansName
+# Copyright (C) $yearp The HTMLDigger
 
-# Change Permition 
-
-sudo chmod a+x $ansName
+#****************************************************************************
+BCyan='\033[1;36m'
+green='\e[92m'
+cyan='\e[36m'
+lightblue='\e[94m'
+off='\033[0m'
+purple='\033[0;35m'
+blink='\033[5m'
+red='\033[31m'
+banner() {
+    clear
+    echo -e "'${cyan}'╦ ╦╔╦╗╔╦╗╦  ╔╦╗┬┌─┐┌─┐┌─┐┬─┐'${off}'"
+    echo -e "'${cyan}'╠═╣ ║ ║║║║   ║║││ ┬│ ┬├┤ ├┬┘'${off}'"
+    echo -e "'${cyan}'╩ ╩ ╩ ╩ ╩╩═╝═╩╝┴└─┘└─┘└─┘┴└─.in'${off}'"
+    echo ""
+}
+nowdate='$(date +"%m-%d-%Y : %r")'
+banner
+echo -e "'"Todays Date : $nowdate"'"
+##########################  YOUR SCRIPT START HERE ##########################
+" >> /$HOME/bash/$ansName
 
 # Script END
